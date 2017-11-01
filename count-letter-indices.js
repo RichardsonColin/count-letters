0a17a826e066baa5918f879d9c7f109bcff00597
Colin Richardson <richardsonjcolin@gmail.com>
2017-11-01 20:33:32 +0000

    functions properly

--- count-letter-indices.js
+++ count-letter-indices.js
@@ -1,13 +1,19 @@
 function countLetters(string){
-  var noSpaceString = string.toLowerCase().split(' ').join('');
+  var noSpaceString = string.toLowerCase();
   console.log(noSpaceString);
   var outputObject = {};
 
   for(index in noSpaceString){
     if(outputObject[noSpaceString[index]] === undefined){
-      outputObject[noSpaceString[index]] = 1;
+      if (noSpaceString[index] === " ")
+        continue;
+      outputObject[noSpaceString[index]] = {
+        frequency: 1,
+        indexes: [index]
+      };
     } else {
-      outputObject[noSpaceString[index]] += 1;
+      outputObject[noSpaceString[index]]["frequency"] += 1;
+      outputObject[noSpaceString[index]]["indexes"].push(index);
     }
   }
   return outputObject;
